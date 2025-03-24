bfea4f4a680682e8cf5d78c5331ab5bd92829a2e
Jye Bussa <jye@tanda.co>
2025-03-24 13:42:25 +1000

    copyrelativepath

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
 
 vim.keymap.set('n', '<leader>gr', ':grep "<C-r><C-w>" app/')
+vim.keymap.set('n', '<leader>cr', function()
+  vim.fn.setreg('+', vim.fn.expand('%'))
+  print("Relative path copied to clipboard")
+end, { desc = "Copy relative file path to clipboard" })
