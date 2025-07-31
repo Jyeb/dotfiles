292e3532cc0c447cce039a2e5a53abe0ee8e146a
Jye Bussa <jye@tanda.co>
2025-07-31 14:43:34 +1000

    lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,42 +9,27 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "solargraph", "ruby_lsp" },
+        ensure_installed = { "lua_ls", "ruby_lsp" },
       })
     end,
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities,
-      })
+      local lspconfig = require('lspconfig')
+
+      -- Lua LSP
+      lspconfig.lua_ls.setup({})
+
+      -- Ruby LSP
       lspconfig.ruby_lsp.setup({
-        capabilities = capabilities,
-        init_options = {
-          formatter = {
-            rubocop = {
-              command = "bundle", -- Use bundled rubocop
-              args = { "exec", "rubocop" },
-            }
-          },
-          diagnostics = {
-            rubocop = {
-              command = "bundle", -- Use bundled rubocop
-              args = { "exec", "rubocop" },
-            }
-          }
-        }
+        cmd = { "env", "RAILS_ENV=test", "ruby-lsp" }
       })
 
+      -- Keymaps
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
 }
